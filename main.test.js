47e6561e2433e0aed9fc9dc83ad0f47987be4c9b
babyyycuttieee <101551909+babyyycuttieee@users.noreply.github.com>
2022-06-19 05:24:50 +0800

    Update main.test.js

--- main.test.js
+++ main.test.js
@@ -3,80 +3,145 @@ const request = supertest('http://localhost:3000');
 const bcrypt = require("bcryptjs");
 
 describe('Express Route Test', function () {
-	// it('should return hello world', async () => {
-	// 	return request.get('/hello')
-	// 		.expect(200)
-	// 		.expect('Content-Type', /text/)
-	// 		.then(res => {
-	// 			expect(res.text).toBe('Hello BENR2423');
-	// 		});
-	// })
-
-	it('User login successfully', async () => {
+//////////////////////////////////////////////LOGIN\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+/////----------------STAFF----------------\\\\\
+	it('Staff login successfully', async () => {
 		return request
-			.post('/login')
-			.send({username: "Salvador Kozey", userpassword: "rIfr22Me1ZnAL2w"})
-			.expect('Content-Type', /text/)
+			.post('/login/user')
+			.send({username: "Robyn Deckow", userpassword: "y2Wok_h25xfk_Ul"})
+			.expect('Content-Type', /json/)
 			.expect(200).then(response => {
-				expect(response.text).toEqual("Login success");
+				expect(response.body).toEqual(
+					expect.objectContaining({
+						username: expect.any(String),
+						//role: expect.any(String),
+						token: expect.any(String)
+					}));
 			});
 	});
 
-	it('login failed', async () => {
+	it('Staff login failed', async () => {
 		return request
-			.post('/login')
-			.send({username: "Salvador Kozey", userpassword: "012345"})
-			.expect('Content-Type', /text/)
-			.expect(404).then(response => {
-				expect(response.text).toEqual("Invalid Input");
-	});
+			.post('/login/user')
+			.send({username: "Forger", userpassword: "1234"})
+			.expect(404)
 });
-	
-
-	it('register', async () => {
-		return request
-			.post('/register')
-			.send({username: "register sucess", userpassword: "123456"})
-			.expect('Content-Type', /text/)
-			.expect(200).then(response => {
-				expect(response.text).toEqual("Successfully, create new account");
-	});
+/////----------------MANAGEMENT----------------\\\\\  
+it('Management login successfully', async () => {
+	return request
+		.post('/login/security')
+		.send({managername: "Rufus Gerlach", managerpassword: "kOrLkU8mt9buM0J"})
+		.expect('Content-Type', /json/)
+		.expect(200).then(response => {
+			expect(response.body).toEqual(
+				expect.objectContaining({
+					token: expect.any(String)
+				}));
+		});
 });
 
-	it('register failed', async () => {
-		return request
-			.post('/register')
-			.send({username: "user4", userpassword: "123456789"})
-			.expect('Content-Type', /text/)
-			.expect(404).then(response => {
-				expect(response.text).toEqual("Username exists");
-			});
-	});
+it('Management login failed', async () => {
+	return request
+		.post('/login/security')
+		.send({username: "Forger", userpassword: "kOrLkU8mt9buM0"})
+		.expect(404)
+});
 
-	it('update successfully', async () => {
+//////////////////////////////////////////////REGISTER\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+/////----------------STAFF----------------\\\\\
+it('Staff register successfully', async () => {
 		return request
-			.patch('/update')
-			.send({username: "Gwen Willms"})
+			.post('/register/user')
+			.send({username: "register sucesss", userpassword: "123456", name: "Lili", email:"afi1234@gmail.com", phonenumber: "60123890154", staffunit: "998670235",role: "staff"})
 			.expect(200)
-	});
-
-	it('delete successfully', async () => {
-    	return request
-    	  .delete('/delete')
-    	  .send({username:"Felix Rogahn", userpassword: "l2S8qohithdNNoQ"})
-    	  .expect('Content-Type', /text/)
-    	  .expect(200).then(response => {
-     			expect(response.text).toEqual("Successfully Delete");
-			});
-	});
-
-  	it('delete failed', async () => {
-    	return request
-    	.delete('/delete')
-      	.send({username: 'delete failed', userpassword: "1010"})
-      	.expect('Content-Type', /text/)
-      	.expect(404).then(response => {
-        		expect(response.text).toEqual("Delete Failed");
-    	});
-	});
 });
+
+it('Staff register failed', async () => {
+		return request
+			.post('/register/user')
+			.send({username: "user4", userpassword: "123456789", name: 'fir', email:"ray654@gmail.com", phonenumber: "60135673009",staffunit: "874320184",role: "staff"})
+			.expect(404)
+});
+/////----------------CUSTOMER----------------\\\\\
+it('Customer register successfully', async () => {
+	return request
+		.post('/register/visitor')
+		.send({custname: "register sucess", custemail:"afi1234@gmail.com", custphonenumber: "6012-3890154",date: '2022-03-19',ticketbooking: '4', role: "customer"})
+		.expect(200)
+});
+
+it('Customer register failed', async () => {
+	return request
+		.post('/register/visitor')
+		.send({username: "user4", custemail:"ray654@gmail.com", custphonenumber: "6013-5673009",date: '2022-08-23',ticketbooking: '6', role: "customer"})
+		.expect(404)
+});
+
+//////////////////////////////////////////////UPDATE\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+/////----------------VISITOR----------------\\\\\
+it('Update ticket booking successfully', async () => {
+		return request
+			.patch('/update/visitor/booking')
+			.send({custname: "Bernadette Lueilwitz DVM", ticketbooking: "3"})
+			.expect(200)
+});
+//////////////////////////////////////////////DELETE\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+/////----------------STAFF----------------\\\\\
+	it('Staff delete successfully', async () => {
+    	return request
+    	  .delete('/delete/user')
+    	  .send({username:"Beatrice Baumbach", userpassword: "HzynxG_WHkkt6wQ"})
+    	  .expect(200)
+	});
+
+  	it('Staff delete failed', async () => {
+    	return request
+    	.delete('/delete/user')
+      	.send({username: 'delete failed', userpassword: "1010"})
+      	.expect(404)
+	});
+/////----------------VISITOR----------------\\\\\
+it('Visitor delete successfully', async () => {
+	return request
+	  .delete('/delete/visitor')
+	  .send({custname:"Tabitha Reinger"})
+	  .expect(200)
+});
+it('Visitor delete failed', async () => {
+	return request
+	  .delete('/delete/visitor')
+	  .send({custname:"Anya"})
+	  .expect(404)
+});
+});
+//////////////////////////////////////////////VIEW\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+/////----------------STAFF----------------\\\\\
+it('view staff', async()=>{
+    return request.get('/view/user/Hugh Schamberger')
+    .expect(200)
+  });
+/////----------------VISITOR----------------\\\\\
+it('view customer', async()=>{
+    return request.get('/view/visitor/Juanita Farrell DVM')
+    .expect(200)
+  });
+
+const jwt = require('jsonwebtoken');
+
+function generateAccessToken(payload) {
+  return jwt.sign(payload, "ainincantik", {expiresIn:'1h'});
+}
+function verifyToken(req,res,next){
+  const authHeader=req.headers['authorization']
+  const token = authHeader && authHeader.split(' ')[1]
+
+  jwt.verify(token, "ainincantik", (err,user)=>{
+    console.log(err)
+
+    if (err) return res.sendStatus(403)
+
+    req.user = user
+
+    next()
+  })
+}
